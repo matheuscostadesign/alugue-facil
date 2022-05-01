d5d8081f491f232bcda15423f9c24ee62abe885c
Matheus Costa <matheuscostadosantos@hotmail.com>
2022-05-01 12:48:35 -0300

    Melhorias layout index

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
           </div>
         </div>
       </div>
-      <div class="row gx-5 row-cols-2 row-cols-lg-4 py-5">
+      <div class="row gx-5 row-cols-2 row-cols-lg-4 py-2">
         <div class="col">
           <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-chat-dots"></i></div>
           <div class="h5 mb-2">Casas</div>
@@ -77,31 +77,36 @@
           <p class="text-muted mb-0">Precisa de uma moto? Aqui também você encontra!</p>
         </div>
       </div>
+      <br>
     </div>
 
-    <section class="py-5 bg-light" id="scroll-target">
-      <div class="container px-5 my-5">
-        <div class="row gx-5 align-items-center">
-          <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
-          <div class="col-lg-6">
-            <h2 class="fw-bolder">Sem burocracias</h2>
-            <p class="lead fw-normal text-muted mb-0">Chega de dor de cabeça na hora de encontrar um produto ou serviço para alugar.</p>
+    <div class="container px-5">
+      <section class="py-5">
+        <div class="container px-2 my-2">
+          <div class="row gx-5 align-items-center">
+            <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
+            <div class="col-lg-6">
+              <h2 class="fw-bolder">Sem burocracias</h2>
+              <p class="lead fw-normal text-muted mb-0">Chega de dor de cabeça na hora de encontrar um produto ou serviço para alugar.</p>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-    <!-- About section two-->
-    <section class="py-5">
-      <div class="container px-5 my-5">
-        <div class="row gx-5 align-items-center">
-          <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
-          <div class="col-lg-6">
-            <h2 class="fw-bolder">Sem custos</h2>
-            <p class="lead fw-normal text-muted mb-0">Faça seu cadastro e encontre ou divulgue seu produto ou serviço sem custo nenhum.</p>
+      </section>
+    </div>
+
+    <div class="container px-5">
+      <section class="py-5">
+        <div class="container px-2 my-2">
+          <div class="row gx-5 align-items-center">
+            <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
+            <div class="col-lg-6">
+              <h2 class="fw-bolder">Sem custos</h2>
+              <p class="lead fw-normal text-muted mb-0">Faça seu cadastro e encontre ou divulgue seu produto ou serviço sem custo nenhum.</p>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
 
     <div class="container px-5 my-5">
       <aside class="bg-green bg-gradient rounded-3 p-4 p-sm-5 mt-5">
@@ -112,7 +117,7 @@
           </div>
           <div class="ms-xl-4">
             <div class="input-group mb-2">
-              <a class="btn btn-outline-light btn-lg px-4" href="/usuarios/cadastrar.php">Quero me cadastrar!</a>
+              <a class="btn btn-outline-light btn-lg px-4" href="/usuarios/cadastrar.php">Cadastrar!</a>
             </div>
           </div>
         </div>
