381099fe8a51a66b73e6c4dd14266be8b585c297
Matheus Costa <matheuscostadosantos@hotmail.com>
2022-05-03 17:39:29 -0300

    SEO Index

--- index.php
+++ index.php
@@ -3,11 +3,51 @@
 
 <head>
 
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://metatags.io/">
+  <meta property="og:title" content="Alugue Fácil - Alugue qualquer coisa sem burocracias">
+  <meta property="og:description" content="Alugue qualquer coisa diretamente com o proprietário">
+  <meta property="og:image" content="https://projetos-senai.netlify.app/uc08/projeto-integrador/alugue-facil/imgs/og-image.png">
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://metatags.io/">
+  <meta property="twitter:title" content="Alugue Fácil - Alugue qualquer coisa sem burocracias">
+  <meta property="twitter:description" content="Alugue qualquer coisa diretamente com o proprietário">
+  <meta property="twitter:image" content="https://projetos-senai.netlify.app/uc08/projeto-integrador/alugue-facil/imgs/og-image.png">
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;600&display=swap" rel="stylesheet">
+
+  <!-- Favicon -->
+  <link rel="apple-touch-icon" sizes="57x57" href="/imgs/favicon/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/imgs/favicon/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/imgs/favicon/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/imgs/favicon/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/imgs/favicon/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/imgs/favicon/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/imgs/favicon/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/imgs/favicon/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/imgs/favicon/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="/imgs/favicon/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/imgs/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/imgs/favicon/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/imgs/favicon/favicon-16x16.png">
+  <meta name="msapplication-TileColor" content="#00ab44">
+  <meta name="msapplication-TileImage" content="/imgs/favicon/ms-icon-144x144.png">
+  <meta name="theme-color" content="#00ab44">
 
   <!-- Styles -->
-  <?php include 'componentes/head-styles.php'; ?>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/css/responsive.css">
 
   <title>Alugue Fácil - Alugue qualquer coisa sem burocracias</title>
   <meta name="title" content="Alugue Fácil - Alugue qualquer coisa sem burocracias">
